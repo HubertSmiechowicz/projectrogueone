d09a244e14c240fe3e2233e2b18b9032cffbc8fd
Hubert Smiechowicz <hubert.smiechowicz@gmail.com>
2025-07-02 19:25:04 +0200

    player basic movement

---
+++ scenes/player/Player.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+using static Godot.TextServer;
+
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	public float Speed = 200f;
+	[Export]
+	public float JumpVelocity = 400f;
+	private Vector2 _velocity = Vector2.Zero;
+
+	AnimationTree _animationTree;
+
+	public override void _Ready()
+	{
+		// Initialization code can go here if needed
+		_animationTree = GetNode<AnimationTree>("AnimationTree");
+
+		_animationTree.Active = true;
+		_animationTree.Set("parameters/conditions/move", true);
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		_velocity.X = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+		_velocity.X *= Speed;
+		if (IsOnFloor())
+		{
+			_animationTree.Set("parameters/conditions/move", true);
+			_animationTree.Set("parameters/conditions/jump", false);
+
+			if (Input.IsActionJustPressed("jump"))
+			{
+				_velocity.Y = -JumpVelocity;
+				_animationTree.Set("parameters/conditions/move", false);
+				_animationTree.Set("parameters/conditions/jump", true);
+			}
+		}
+		else
+		{
+			_velocity.Y += GetGravity().Y * (float)delta; // Gravity
+			_animationTree.Set("parameters/Jump/blend_position", _velocity.X);
+		}
+
+		Velocity = _velocity;
+
+		_animationTree.Set("parameters/Move/blend_position", _velocity.X);
+
+		MoveAndSlide();
+	}
+}
